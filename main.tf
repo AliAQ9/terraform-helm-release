ef314820e62ee7f6c8bb81691d05ff423a32a047
AliAQ9 <qep5319@gmail.com>
2024-06-21 18:34:46 -0500

    fixed module

--- main.tf
+++ main.tf
@@ -1,59 +1,16 @@
 module "app" {
-    source = "./module"
-    namespace = "default"
-    name = "wordpress"
-    wait = "false"
-    chart = "./application"
-    values = [<<EOF
-
+  source    = "./module"
+  namespace = "default"
+  name      = "wordpress"
+  wait      = false
+  chart     = "./application"
+  values = [<<EOF
 replicaCount: 3
-
 image:
   repository: wordpress
   pullPolicy: IfNotPresent
   # Overrides the image tag whose default is the chart appVersion.
   tag: "latest"
-
-EOF
-]
-}
-
-module "python" {
-    source = "./module"
-    namespace = "default"
-    name = "python"
-    wait = "false"
-    chart = "./application"
-    values = [<<EOF
-
-replicaCount: 3
-
-image:
-  repository: python
-  pullPolicy: IfNotPresent
-  # Overrides the image tag whose default is the chart appVersion.
-  tag: "latest"
-
-EOF
-]
-}
-
-module "nodejs" {
-    source = "./module"
-    namespace = "default"
-    name = "nodejs"
-    wait = "false"
-    chart = "./application"
-    values = [<<EOF
-
-replicaCount: 3
-
-image:
-  repository: wordpress
-  pullPolicy: IfNotPresent
-  # Overrides the image tag whose default is the chart appVersion.
-  tag: "latest"
-
-EOF
-]
+  EOF
+  ]
 }
--- module/variable.tf
+++ module/variable.tf
@@ -1,7 +1,29 @@
-resource "helm_release" "wordpress" {
-  name   = var.name
-  namespace = var.namespace
-  chart  = var.chart
-  wait   = var.wait
-  values = var.values
+variable "name" {
+  description = "The name of the app"
+  type        = string
+  default     = "wordpress"
+}
+
+variable "namespace" {
+  description = "The namespace of the app"
+  type        = string
+  default     = "default"
+}
+
+variable "chart" {
+  description = "The chart of the app"
+  type        = string
+  default     = "../application"
+}
+
+variable "wait" {
+  description = "Wait for the resource to be ready"
+  type        = bool
+  default     = false
+}
+
+variable "values" {
+  description = "The values of the app"
+  type        = list(string)
+  default = []
 }
